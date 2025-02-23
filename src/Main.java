f26856d0c7ec8e82f6b733158287a8dab87a9a4b
E-Albert <magicspoon123@gmail.com>
2025-02-23 17:22:19 -0500

    Created Main class which starts application when ran

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //creates LMS object
+        DriverManager driverManager = new DriverManager();
+        //calls LMS method to start user interaction
+        driverManager.menu();
+    }
+}
